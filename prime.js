7b57de3dfd333b4b9d64090ae303ef919e4d738d
Kumkum kanojiy <kumkumkanojiya22@gmail.com>
2022-09-20 17:14:48 -0400

    fix by kum

--- prime.js
+++ prime.js
@@ -1,5 +1,16 @@
-function checkprime(){
-
- for(let i = 0; i <= 
+function checkprime(num){
+  let fac=0
+ for(let i = 0; i <= 10; i++){
+   if(num%i==0){
+      fac=fac+1
+   }
+ }
+ if(fac===2){
+  console.log("prime")}
+ else{
+  console.log(" not prime")
+     }
+    
 
 }
+checkprime(5);
